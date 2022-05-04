37e4f6ad94f6ac4144153b386d3d48910974495b
Tahsin Soyak <54424377+tahsinsoyak@users.noreply.github.com>
2022-05-04 04:20:48 +0300

    UDP Sockets [2 process ile veri gönderme ve alma]

---
+++ 26_udp.py
@@ -0,0 +1,61 @@
+#UDP Sockets
+#Tek taraflı handshake yok
+
+
+"""
+UDP (User Datagram Protocol) is a communications protocol
+that is primarily used for establishing low-latency and loss-tolerating
+connections between applications on the internet
+It speeds up transmissions by enabling the transfer of data before an
+agreement is provided by the receiving party.
+"""
+
+#Imports
+from audioop import add
+import logging
+import multiprocessing #prosesin biri dinleyip biri yayın yapacak
+import threading
+import socket
+import sys
+import time
+logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s',datefmt='%H:%M:%S',level=logging.DEBUG)
+
+
+#Socket
+def make_socket(ip='localhost',port=2045,sender=False):
+    proc = multiprocessing.current_process().name
+    logging.info(f'{proc}: starting')
+    
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    if sender:
+        logging.info(f'{proc}: starting to send')
+    else:
+        logging.info(f'{proc}: binding to port')
+        address = (ip,port)
+        s.bind(address)
+
+    with s:
+        while True:
+            if sender:
+                logging.info(f'{proc}: sending...')
+                s.sendto(b'Hello UDP',(ip,port))
+                time.sleep(1)
+            else:
+                data,addr = s.recvfrom(1024)
+                logging.info(f'{proc}: from {addr} = {data}')
+
+
+
+
+def main():
+    broadcaster = multiprocessing.Process(target=make_socket,kwargs={'sender':True},daemon=True,name='Broadcaster')
+    listener = multiprocessing.Process(target=make_socket,kwargs={'sender':False},daemon=True,name='Listener')
+
+    broadcaster.start()
+    listener.start()
+
+    timer = threading.Timer(5,sys.exit,[0])
+    timer.start()
+
+if __name__ == "__main__":
+    main()
